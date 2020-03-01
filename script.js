2e53607a7b3e364e5e736ab45c8896f136a04a15
Jenny Sugden <jensugden@gmail.com>
2020-03-01 12:30:24 -0800

    revised bank app code

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-let welcome = prompt('Select Quit, Withdraw, Deposit, or Balance');
+let bankOpen = true;
+let balance = 500;
 
-let bankOption = 'Select Quit, Withdraw, Deposit, or Balance';
+do {
+  const welcome = prompt("Select (Q)uit, (W)ithdraw, (D)eposit, or (B)alance");
 
-switch(welcome) {
-  case "Quit":
-    text = "Thank you for banking with us. GoodBye!";
-    alert(text);
-    break;
-  case "Balance":
-    total = 'Your balance is $500.'
-    alert(total);
-    prompt(bankOption);
-  case "Withdraw":
-    text = "How much would you like to withdraw?";
-    total = 'Your balance is $500.'
-    prompt(text);
-    alert(total);
-    prompt(bankOption);
-  case "Deposit":
-    text = 'How much would you like to deposit?';
-    total = 'Your balance is $500.'
-    prompt(text);
-    alert(total);
-    prompt(bankOption);
-}
+  switch(welcome) {
+    case "q":
+      bankOpen = false;
+      alert('Thank you for banking with us!');
+      break;
+    case "b":
+      text = 'Your balance is $' + balance.toString();
+      alert(text);
+      break;
+    case "d":
+      const depositAmount = Number(prompt('How much would you like to deposit?'));
+      balance = depositAmount + balance;
+      if (balance >= 50000) {
+        alert('Sorry, please deposit less than $50,000');
+      } else if (balance < 50000) {
+        alert('Thank you! Your new balance is $' + balance);
+      }
+      break;
+    case "w":
+      const withdrawAmount = Number(prompt('How much would you like to withdraw?'));
+      balance = balance - withdrawAmount;
+      if (balance <= 0) {
+        alert('Sorry, your withdraw is greater than your balance.')
+      } else if (balance <= 299) {
+        alert('Your new balance will be less than $300. Do you want to proceed?');
+      } else if (balance >= 1) {
+        alert('Thank you! Your new balance is $' + balance);
+      }
+      break;
+    default:
+      alert('Enter the appropriate option.');
+  }
+} while (bankOpen === true);
